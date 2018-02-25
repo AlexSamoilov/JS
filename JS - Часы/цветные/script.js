ad2ef8744aca4bafee13d9fdece78cb8fa801571
Alexandr <Gemini_1982@mail.ru>
2018-02-25 23:03:47 +0200

    color clock

---
+++ JS - Часы/цветные/script.js
@@ -0,0 +1,39 @@
+var clock = document.getElementById('clock');
+var color = document.getElementById('color');
+
+function hexoClock () {
+    var time = new Date ();
+    var h = (time.getHours() % 12).toString();
+    var m = time.getMinutes().toString();
+    var s = time.getSeconds().toString();
+
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (s.length < 2) {
+        s = '0' + s;
+    }
+
+    var clockString = h + ':' + m + ':' + s;
+    var colorString = '#' + h + m + s;
+
+    // clock.textContent = clockString;
+    // color.textContent = colorString;
+
+    clock.innerText = clockString;
+    color.innerText = colorString;
+
+    document.body.style.background = colorString;
+}
+
+// function hexoClock () {
+//     var time = new Date ();
+//     clock.innerText = time;
+// }
+
+hexoClock();
+setInterval(hexoClock, 1000);
+
