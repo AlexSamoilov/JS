d0ede24f2676706a44c1cb65c08642090f720cdc
Alexandr <Gemini_1982@mail.ru>
2018-03-13 17:00:11 +0200

    DZ 5

---
+++ JS 18-02/DZ  5/script.js
@@ -0,0 +1,16 @@
+// Сделать картинку которая перемещается по экрану, 
+// можно плавно можно нет.
+
+var elem = document.getElementById("elem");
+var step = 0;
+var id = setInterval(move, 100);
+function move () {
+	if (step >= 80) {
+		step = 0;
+	} 
+	else {
+		step++;
+		elem.style.left = step + '%';
+		elem.style.top = step + '%';
+	}
+}
