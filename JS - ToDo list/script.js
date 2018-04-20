ca111a7bcafc2a25dc7606824ea54dc5215d4c7a
Alexandr <Gemini_1982@mail.ru>
2018-04-21 00:12:40 +0300

    last JS changes

--- JS - ToDo list/script.js
+++ JS - ToDo list/script.js
@@ -1,53 +1,46 @@
-var myNodelist = document.getElementByTagName ('LI');
-var i;
-for (i = 0; i < myNodelist.length; i++) {
- 	var span = document.createElement("SPAN");
- 	var txt = document.createTextNode("\u00D7");
- 	span.className = "close";
- 	span.appendChild(txt);
- 	myNodelist[i].appendChild(span);
+//Создаем у всех li кнопки close (крестик)
+var myNodelist = document.getElementsByTagName('li');
+for (var i = 0; i < myNodelist.length; i++) {
+  createCloseButton(myNodelist[i]);
 }
 
-var close = document.getElementByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
- 	close[i].onclick = function() {
-  	var div = this.parentElement;
-  	div.style.display = "none";
- 	}
-}
-
+//Добавляем выделение при клике на li
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-	if (ev.target.tagName === 'LI') {
-  	ev.target.classList.toggle('checked');
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
   }
 }, false);
 
+//Создание нового элемента из данных в myInput
 function newElement() {
-	var li = document.createElement("li");
-	var inputValue = document.getElementById("myInput").value;
-	var t = document.createTextNode(inputValue);
-	li.appendChild(t);
-	if (inputValue === '') {
-		alert("Поле не может быть пустым!");
-	}
-	else {
-		document.getElementById("myUL").appendChild(li);
-	}
-document.getElementById("myInput").value = "";
-	
-	var span = document.createElement("SPAN");
- 	var txt = document.createTextNode("\u00D7");
- 	span.className = "close";
- 	span.appendChild(txt);
- 	li.appendChild(span);
+  var inputValue = document.getElementById("myInput").value;
+  if (inputValue === '') {
+    alert("The field dont't be empty!");
+  } 
+  else {
+  //Создаем новый элемент li
+    var li = document.createElement("li");
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById('myUL').appendChild(li);
 
- 	for (i = 0; i < close.length; i++) {
- 	close[i].onclick = function() {
-  	var div = this.parentElement;
-  	div.style.display = "none";
- 		}
-	}
+    //Делаем кнопку для удаления (крестик)
+    createCloseButton(li);
+
+    //Чистим поле input
+    document.getElementById('myInput').value = "";
+  }
 }
 
+function createCloseButton(node) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  node.appendChild(span);
+  span.onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
