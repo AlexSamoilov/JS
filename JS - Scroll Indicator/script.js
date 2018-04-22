6b38bf959793a149f4f309b46506b6e7c785db7c
Alexandr <Gemini_1982@mail.ru>
2018-04-22 18:57:13 +0300

    Scroll Indicator

---
+++ JS - Scroll Indicator/script.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {
+	docScroll();
+}
+
+function docScroll() {
+let windowScroll = document.body.scrollTop || document.documentElement.scrollTop,
+	documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+	scrolled = (windowScroll / documentHeight) * 100;
+
+	document.getElementById('myBar').style.width = scrolled + '%';
+}
