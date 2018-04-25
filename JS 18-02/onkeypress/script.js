37ddc302d6e4392e62989504142f03a616480609
Alexandr <Gemini_1982@mail.ru>
2018-04-25 15:19:42 +0300

    only numbers in Input

--- JS 18-02/onkeypress/script.js
+++ JS 18-02/onkeypress/script.js
@@ -1,7 +1,15 @@
-document.onkeypress = function(event) {
-	console.log(event.Key);
-	console.log(event);
-	if (event.shiftKey) {
-		console.log("нажата клавиша Shift");
+// document.onkeypress = function(event) {
+// 	console.log(event.Key);
+// 	console.log(event);
+// 	if (event.shiftKey) {
+// 		console.log("нажата клавиша Shift");
+// 	}
+// }
+
+document.getElementById('test').onkeypress = function(event){
+	//console.log(event);
+	if (event.keyCode<48 || event.keyCode>57) {
+		console.log('не цифра');
+		return false;
 	}
 }
