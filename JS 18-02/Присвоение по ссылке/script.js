0ad505086f3f34c34f1c68bf2a720c6f31bd77be
Alexandr <Gemini_1982@mail.ru>
2018-05-09 15:34:32 +0300

    simple constructor

--- JS 18-02/Присвоение по ссылке/script.js
+++ JS 18-02/Присвоение по ссылке/script.js
@@ -23,3 +23,20 @@ for (let key in a) {
 b.five = 5;
 console.log(a);
 console.log(b);
+
+//простой конструктор
+
+class Test {
+	constructor (a){
+		this.c = a;
+	}
+}
+
+let myObj = new Test (5);
+myObj.d = 77;
+console.log(myObj);
+
+let myObj2 = new Test (6);
+console.log(myObj2);
+
+//* Помнить - При присвоении обьекта, не создается новый обьект, а ссылаемся на старый.
